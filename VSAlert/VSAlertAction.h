f7b476b34eedc0c56136feb03dcba43815672799
Varun Santhanam <varun.santhanam@sherbit.io>
2017-10-10 11:21:17 -0700

    re-doing documentation starts now!

--- VSAlert/VSAlertAction.h
+++ VSAlert/VSAlertAction.h
@@ -7,90 +7,28 @@
 
 @import UIKit;
 
-/**
- An enuration for the various UI styles of VSAlertAction
-
- - VSAlertActionStyleDefault: Standard action UI
- - VSAlertActionStyleCancel: Slightly bolder action UI, use for actions that will "cancel"
- - VSAlertActionStyleDestructive: Red action UI, use for actions that will break or ireversably cahgne something
- */
 typedef NS_ENUM(NSInteger, VSAlertActionStyle) {
     
-    /**
-     Standard action UI
-     */
     VSAlertActionStyleDefault,
-    
-    /**
-     Slightly bolder action UI, use for actions that will "cancel"
-     */
     VSAlertActionStyleCancel,
-    
-    /**
-     Red action UI, use for actions that will break or ireversably cahgne something
-     */
     VSAlertActionStyleDestructive
     
 };
 
-/**
- VSAlertAction is a specialized subclass used to configure a VSAlertController.
- */
 @interface VSAlertAction : UIButton
 
-/**
- The font used by alert actions that are not styled as "cancel". If none is provided, the system font at regular weight and size 17.0f is used. (Class property, applies to all instances created after change)
- */
-@property (class, strong, nullable) UIFont *actionTitleTextFont;
-
-/**
- The font used by alert actions that are styled as "cancel". If none is provided, the system font at semibold weight and size 17.0f is used. (Class property, applies to all instances created after change)
- */
 @property (class, strong, nullable) UIFont *cancelActionTitleTextFont;
-
-/**
- The text color used by alert actinos that are not styled as "destructive". If none is provided, UIColor's `+redColor` is used. (Class property, applies to all instances created after change).
- */
+@property (class, strong, nullable) UIFont *actionTitleTextFont;
 @property (class, strong, nullable) UIColor *actionTextColor;
-
-/**
- The text color used by alert actions that are not styled as "destructive". If none is provided, the button's `.tintColor` is used.
- */
 @property (class, strong, nullable) UIColor *destructiveActionTextColor;
 
-/**
- The block to be executed when the action is selected by the user
- */
 @property (NS_NONATOMIC_IOSONLY, copy, nullable) void (^action)(VSAlertAction * _Nonnull action);
-
-/**
- The the UI style of the action
- */
 @property (NS_NONATOMIC_IOSONLY, assign, readonly) VSAlertActionStyle style;
 
-/**
- Factory method to create an alert action
-
- @param title Title of the action
- @param style The style of the action
- @param action The block to be executed when the user taps on an action
- @return The action
- */
 + (nullable instancetype)alertActionWithTitle:(nonnull NSString *)title style:(VSAlertActionStyle)style action:(void (^_Nullable)(VSAlertAction * _Nonnull action))action;
 
-/**
- Reset class property customization to its default settings
- */
 + (void)resetStyleToDefaults;
 
-/**
- Create an action
-
- @param title Title of the action
- @param style The style of the action
- @param action The block to be executed when the user taps on an action
- @return The action
- */
 - (nullable instancetype)initWithTitle:(nonnull NSString *)title style:(VSAlertActionStyle)style action:(void (^_Nullable)(VSAlertAction *_Nonnull))action NS_DESIGNATED_INITIALIZER;
 
 @end
--- VSAlert/VSAlertController.h
+++ VSAlert/VSAlertController.h
@@ -8,169 +8,48 @@
 @import UIKit;
 
 #import "VSAlertAction.h"
-/**
- An exception thrown when an unimplemented feature is used at runtime.
- */
-extern NSString * _Nonnull const VSAlertControllerNotImplementedException;
 
-/**
- An exceptino thrown when a textfield is added in an alertcontroller style which doesn't support text fields
- */
+extern NSString * _Nonnull const VSAlertControllerNotImplementedException;
 extern NSString * _Nonnull const VSAlertControllerTextFieldInvalidException;
 
-/**
- An enuration for the various UI styles of VSAlertController
-
- - VSAlertControllerStyleAlert: A pop up alert that displays at 270px wide (like Apple's UIAlertController)
- - VSAlertControllerStyleWalkthroughAlert: A wider popup that stretches it's to fit the width of the device (minus some padding)
- - VSAlertControllerStyleActionSheet: A bottom of the screen alert, similar to Apple's Action Sheet style
- */
 typedef NS_ENUM(NSInteger, VSAlertControllerStyle) {
 
-    /**
-     A pop up alert that displays at 270px wide (like Apple's UIAlertController)
-     */
     VSAlertControllerStyleAlert,
-    
-    /**
-     A wider popup that stretches it's to fit the width of the device (minus some padding)
-     */
     VSAlertControllerStyleWalkthroughAlert,
-    
-    /**
-     A bottom of the screen alert, similar to Apple's Action Sheet style
-     */
     VSAlertControllerStyleActionSheet
     
 };
 
-/**
- An enumeration for the various animation styles
 
- - VSAlertControllerPresentAnimationStyleRise: The alert rises from the bottom of the device
- - VSAlertControllerPresentAnimationStyleFall: The alert falls from the top of the device
- - VSAlertControllerPresentAnimationStyleSlide: The alert slides in from the left of the device
- - VSAlertControllerPresentAnimationStyleFlip: The alert flips from the left
- */
 typedef NS_ENUM(NSInteger, VSAlertControllerAnimationStyle) {
     
-    /**
-     The alert rises from the bottom of the device
-     */
     VSAlertControllerAnimationStyleRise,
-    
-    /**
-     The alert falls from the top of the device
-     */
     VSAlertControllerAnimationStyleFall,
-    
-    /**
-     The alert slides in from the left of the device
-     */
     VSAlertControllerAnimationStyleSlide,
-    
-    /**
-     The alert flips from the left
-     */
     VSAlertControllerAnimationStyleFlip
     
 };
 
-/**
- VSAlertController is a drop-in replacement for UIAlertController with a bit more customization and better looks. It's largely based on Codeido's PMAlertController, but has been re-written in Objective-C rather than Swift, and supports a few more features like dynamically sorted alert actions and a "destructive" action style that was missing in PMAlertController. It doesn't requrie a separate XIB file for better potability, and uses the system font by default for a more vanilla look. VSAlertController can can be customized at the class level, so you can make any/all UI changes once.
- */
+
 @interface VSAlertController : UIViewController
 
-/**
- @name Class Properties
- */
-
-/**
-The color of the title text used by alert controllers. The default value is nil. If nil is specified when an alert controller is displayed, VSAlertController.textColor's value is used instead. (Class property, applies to all instances created after change)
-*/
 @property (class, strong, nullable) UIColor *titleTextColor;
-
-/**
- The color of the text used by alert controllers. (Class property, applies to all instances created after change)
- */
 @property (class, strong, nonnull) UIColor *textColor;
-
-/**
- The font of the title used by alert controllers. The default value is the system font at size 17.0f. (Class property, applies to all instances created after change)
- */
 @property (class, strong, nonnull) UIFont *titleTextFont;
-
-/**
- The font of the text used by alert controllers. The default value is the the system font at size 15.0f (Class property, applies to all instances created after change)
- */
 @property (class, strong, nonnull) UIFont *textFont;
 
-/**
- @name Instance Properties
- */
-
-/**
- An array of UITextFields visible on the alert
- */
 @property (NS_NONATOMIC_IOSONLY, strong, readonly, nonnull) NSArray<UITextField *> *textFields;
-
-/**
- YES if the alert controller should dismiss itself when the background is tapped, otherwise NO (default is no)
- */
 @property (NS_NONATOMIC_IOSONLY, assign, getter=shouldDismissOnBackgroundTap) BOOL dismissOnBackgroundTap;
-
-/**
- The animation style to use when the alert is presented
- */
 @property (NS_NONATOMIC_IOSONLY, assign) VSAlertControllerAnimationStyle animationStyle;
 
-/**
- @name Class Methods
- */
-
-/**
- A factory method to create a new alert. The alert is dynamically sized based on your initialization paramaters
-
- @param title The title of the alert.
- @param description The message or body of the alert.
- @param image The image to show on the alert, if any. See the provided image template for details.
- @param style The UI style of the alert
- @return The initialized alert controller.
- */
 + (nullable instancetype)alertControllerWithTitle:(nullable NSString *)title description:(nullable NSString *)description image:(nullable UIImage *)image style:(VSAlertControllerStyle)style;
 
-/**
- Reset class property customization to its default settings
- */
 + (void)resetStyleToDefaults;
 
-/**
- @name Instance Methods
- */
 
-/**
- Create a new alert controller
-
- @param title The title of the alert.
- @param description The message or body of the alert.
- @param image The image to show on the alert, if any. See the provided image template for details.
- @param style The UI style of the alert
- @return The initialized alert controller.
- */
 - (nullable instancetype)initWithTitle:(nullable NSString *)title description:(nullable NSString *)description image:(nullable UIImage *)image style:(VSAlertControllerStyle)style NS_DESIGNATED_INITIALIZER;
 
-/**
- Add an action to the alert controller. See VSAlertAction's documentation for more information
-
- @param alertAction The action to add to the controller
- */
 - (void)addAction:(nonnull VSAlertAction *)alertAction;
-
-/**
- Add a text field to the alert controller. Rather than instantiating the UITextField yourself, use the configuration block (which provides you an already initialized UITextField, and make your customizations there. Unlike in PMAlertController, you can re-assign the the UITextField's delegate property -- VSAlertController does not make use of UITextField to infer when to dismiss and show the keyboard.
-
- @param configuration the block used to configure or style the textfield
- */
 - (void)addTextField:(void (^_Nullable)(UITextField * _Nonnull textField))configuration;
 
 @end
