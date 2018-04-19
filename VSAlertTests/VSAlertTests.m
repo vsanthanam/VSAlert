657ab56bc2f2027f295c50f6f3c79a90cf2fac38
Varun Santhanam <talkto@vsanthanam.com>
2018-04-19 10:02:38 -0700

    tests

--- VSAlertTests/VSAlertTests.m
+++ VSAlertTests/VSAlertTests.m
@@ -24,15 +24,10 @@
 }
 
 - (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    XCTAssert(YES, @"tests are being executed");
+    
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
