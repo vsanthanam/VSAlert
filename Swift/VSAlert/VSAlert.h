08ba8bc5a97a1a6952bcbe22cd167c6bfce69b73
Varun Santhanam <talkto@vsanthanam.com>
2018-06-04 14:04:34 -0700

    added Swift project + targets

---
+++ Swift/VSAlert/VSAlert.h
@@ -0,0 +1,19 @@
+//
+//  VSAlert.h
+//  VSAlert
+//
+//  Created by Varun Santhanam on 6/4/18.
+//  Copyright © 2018 Varun Santhanam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VSAlert.
+FOUNDATION_EXPORT double VSAlertVersionNumber;
+
+//! Project version string for VSAlert.
+FOUNDATION_EXPORT const unsigned char VSAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VSAlert/PublicHeader.h>
+
+
